895a437210f148fb196697b0cd6a75c562a63539
Suyog Soti <suyog.soti@gmail.com>
2019-06-27 15:10:48 -0700

    make it be able to pass context to child

--- sdk/core/azure-core/azure/core/trace/context.py
+++ sdk/core/azure-core/azure/core/trace/context.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 import threading
 from os import environ
 from typing import List
@@ -43,16 +43,16 @@ class ContextAsyc:
         self.contextvar.set(self.default())
 
     def get(self):
-        with self.lock:
-            try:
-                return self.contextvar.get()
-            except LookupError:
-                value = self.default()
-                self.set(value)
-                return value
+        try:
+            return self.contextvar.get()
+        except LookupError:
+            value = self.default()
+            self.set(value)
+            return value
 
     def set(self, value):
-        self.contextvar.set(value)
+        with self.lock:
+            self.contextvar.set(value)
 
 
 class Context:
@@ -67,16 +67,16 @@ class Context:
         setattr(self._thread_local, self.name, self.default())
 
     def get(self):
-        with self.lock:
-            try:
-                return getattr(self._thread_local, self.name)
-            except AttributeError:
-                value = self.default()
-                self.set(value)
-                return value
+        try:
+            return getattr(self._thread_local, self.name)
+        except AttributeError:
+            value = self.default()
+            self.set(value)
+            return value
 
     def set(self, value):
-        setattr(self._thread_local, self.name, value)
+        with self.lock:
+            setattr(self._thread_local, self.name, value)
 
 
 class TracingContext:
@@ -86,6 +86,35 @@ class TracingContext:
     def __init__(self):
         # type: () -> None
         self.current_span = TracingContext.register_slot("current_span", None)
+
+    def with_current_context(self, func):
+        # type: (Callable[Any, Any]) -> Any
+        caller_context = self.get_snapshot()
+
+        def call_with_current_context(*args, **kwargs):
+            try:
+                backup_context = self.get_snapshot()
+                self.apply(caller_context)
+                return func(*args, **kwargs)
+            finally:
+                self.apply(backup_context)
+
+        return call_with_current_context
+
+    def apply(self, contexts):
+        for key in contexts:
+            val = contexts[key]
+            attr = getattr(self, key)
+            attr.set(val)
+
+    def get_snapshot(self):
+        # type: () -> Dict[str, Any]
+        attrs = vars(self)
+        new_dict = {}
+        for key in attrs:
+            if isinstance(attrs[key], TracingContext._context):
+                new_dict[key] = attrs[key].get()
+        return new_dict
 
     @classmethod
     def register_slot(cls, name, default_val):
